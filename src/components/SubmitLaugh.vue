8a4abde07b7cbaa68e6972f340984c69358befea
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-05-18 17:09:38 -0600

    making progress on recording from internal mic. Functions run but I need to work out a few bugs

--- src/components/SubmitLaugh.vue
+++ src/components/SubmitLaugh.vue
@@ -7,12 +7,14 @@
       <label for="description">Description</label>
       <input id=descriptionInput type="text" v-model="submission.description">
        <select @change="chooseMethod" name="method">
+          <option value="Placeholder">Select a submission method</option>
           <option value="Record">Record Your Laugh</option>
           <option value="ChooseFile">Choose Audio Laugh to Upload</option>
           <option value="URL">Link to a laugh URL</option>
       </select> 
       <label v-show="url" for="laughlink">Link to your laugh</label>
       <input v-show="url" id=link type="text" v-model="submission.laughlink">
+      <button v-show="mic">Record Your Laugh</button>
       <form v-show="findFile" id="uploadForm" @submit.prevent="uploadLaugh">
         <h2>{{this.post}}</h2>
         <label for="image">Add Your Laugh Here</label>
@@ -21,6 +23,8 @@
       </form>
       <input  id="submit" type="submit" value="Submit">
     </form>
+    <button @click="startStop" v-show="recordButton">Play Button</button>
+    <button @click="recordAudio" >Mic Test</button>
   </div>
 </template>
 <script>
@@ -29,8 +33,12 @@ export default {
   props: ["laughs", "getListings"],
   data() {
     return {
+      audioChunks: [],
+      recordButton: false,
+      record: false,
       url: false,
       findFile: false,
+      mic: false,
       submission: {
         name: "",
         description: "",
@@ -61,15 +69,53 @@ export default {
       })
         .then(response => response.json())
         .then(response => (this.submission.laughlink = response.audioUrl))
-        .then(() => alert("Laugh Uploaded. Be sure to submit it 😉"));
+        .then(() => alert("Laugh Uploaded. Don't forget to submit it 😉"));
+    },
+    startStop() {
+      this.record = !this.record;
+    },
+    useRecorder(stream) {
+      let mediaRecorder = new MediaRecorder(stream);
+      let start = () => mediaRecorder.start();
+      let stop = () => mediaRecorder.stop();
+      if ((record = true)) {
+        start();
+      } else if ((record = false)) {
+        stop();
+      }
+      mediaRecorder.onstop = function(event) {
+        console.log("data available after MediaRecorder.stop() called.");
+        mediaRecorder.ondataavailable = function(event) {
+          this.audioChunks.push(event.data);
+        };
+        let audioBlob = new Blob(this.audioChunks);
+        this.uploadLaugh(audioBlob);
+        console.log(
+          "If it spit out a link it shoul be here => ",
+          this.submission.laughlink
+        );
+      };
+    },
+    recordAudio() {
+      this.recordButton = true;
+      let stream = navigator.mediaDevices.getUserMedia({
+        audio: true
+      });
+      this.useRecorder(stream);
     },
     chooseMethod() {
       let select = document.querySelector("select");
       if (select.value === "URL") {
         this.url = true;
         this.findFile = false;
+        this.mic = false;
       } else if (select.value === "ChooseFile") {
         this.findFile = true;
+        this.url = false;
+        this.mic = false;
+      } else if (select.value === "Record") {
+        this.mic = true;
+        this.findFile = false;
         this.url = false;
       }
     }
