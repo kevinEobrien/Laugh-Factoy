296cd2e1ba07b4216c3fde2897a0372dc5978ebe
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-05-25 15:22:50 -0600

    :wrench: additional styling fixes

--- src/components/SubmitLaugh.vue
+++ src/components/SubmitLaugh.vue
@@ -1,36 +1,53 @@
 <template>
   <div class="submit-card">
       <h2>Submit a laugh</h2>
-    <form id="form-input" v-on:submit.prevent="submitLaugh">
+    <form id="form-input" >
       <label for="name">Name</label>
       <input id=nameinput type="text" v-model="submission.name">
       <label for="description">Description</label>
       <input id=descriptionInput type="text" v-model="submission.description">
-       <select @change="chooseMethod" name="method">
+      <div class="custom-select">
+        <select @change.prevent="chooseMethod" name="method">
+          <option value="Placeholder">Select a submission method</option>
           <option value="Record">Record Your Laugh</option>
-          <option value="ChooseFile">Choose Audio Laugh to Upload</option>
+          <option class="s-page-button" value="ChooseFile">Choose Audio Laugh to Upload</option>
           <option value="URL">Link to a laugh URL</option>
-      </select> 
+        </select>
+      </div> 
       <label v-show="url" for="laughlink">Link to your laugh</label>
       <input v-show="url" id=link type="text" v-model="submission.laughlink">
       <form v-show="findFile" id="uploadForm" @submit.prevent="uploadLaugh">
         <h2>{{this.post}}</h2>
         <label for="image">Add Your Laugh Here</label>
         <input type="file" name="audio" id="audio">
-          <input  type="submit" name="submit" id="submit" value="Upload">
+          <input  class="s-page-button" type="submit" name="submit" id="submit" value="Upload">
       </form>
-      <input  class="s-page-button" id="submit" type="submit" value="Submit">
+      <img v-show="mic" src="https://media.giphy.com/media/qRhgtheyqGnPW/giphy.gif" width="480" height="361" frameBorder="0">
+      <br>
+       <button class="s-page-button" id="recordLight" @click="stopRecorder" v-show="recordButton">Stop Recording</button>
+      <button class="s-page-button" v-show="mic" @click.prevent="recordAudio">Record</button>
+      <audio v-show="mic" controls></audio>
+      <button class="s-page-button" v-show="readyForUpload" @click="uploadR">Upload Recording</button>
+      <input  @click.prevent="submitLaugh" class="s-page-button" type="submit" value="Submit">
     </form>
   </div>
 </template>
 <script>
 export default {
   name: "SubmitLaugh",
-  props: ["laughs", "getListings"],
+  props: ["laughs"],
   data() {
     return {
+      audioURL: "",
+      audioBlob: undefined,
+      mediaRecorder: {},
+      audioChunks: [],
+      readyForUpload: false,
+      recordButton: false,
+      record: false,
       url: false,
       findFile: false,
+      mic: false,
       submission: {
         name: "",
         description: "",
@@ -41,19 +58,42 @@ export default {
     };
   },
   methods: {
+    getListings() {
+      return fetch(this.apiURL)
+        .then(response => response.json())
+        .then(response => {
+          this.laughs = response.laughs.sort();
+        });
+    },
     submitLaugh() {
-      fetch(this.apiURL, {
-        method: "POST",
-        headers: new Headers({ "Content-Type": "application/json" }),
-        body: JSON.stringify(this.submission)
-      }).then(response => response.json());
-      this.submission.name = "";
-      this.submission.description = "";
-      this.submission.laughlink = "";
-      this.submission.likes = 0;
+      if (
+        this.submission.laughlink === "" ||
+        this.submission.name === "" ||
+        this.submission.description === ""
+      ) {
+        alert(
+          "Please be sure you have filled out a name, description, and provided a laugh in order to submit."
+        );
+      } else {
+        fetch(this.apiURL, {
+          method: "POST",
+          headers: new Headers({ "Content-Type": "application/json" }),
+          body: JSON.stringify(this.submission)
+        }).then(response => response.json());
+        this.submission.name = "";
+        this.submission.description = "";
+        this.submission.laughlink = "";
+        this.submission.likes = 0;
+        this.readyForUpload = false;
+        this.audioURL = "";
+        console.log("gets to end of function");
+        this.getListings();
+        this.$route.router.go("/laughlist");
+      }
     },
     uploadLaugh(event) {
-      console.log(event);
+      console.log("no R");
+      event.stopPropagation();
       fetch("https://vast-wildwood-21026.herokuapp.com/upload", {
         method: "POST",
         body: new FormData(event.target),
@@ -61,21 +101,86 @@ export default {
       })
         .then(response => response.json())
         .then(response => (this.submission.laughlink = response.audioUrl))
-        .then(() => alert("Laugh Uploaded. Be sure to submit it 😉"));
+        .then(() => alert("Laugh Uploaded. Don't forget to submit it 😉"));
+    },
+    uploadR(event) {
+      event.stopPropagation();
+      let formData = new FormData();
+      formData.append("audio", this.audioBlob);
+      fetch("https://vast-wildwood-21026.herokuapp.com/upload", {
+        method: "POST",
+        body: formData,
+        "Content-type": "multipart/form-data"
+      })
+        .then(response => response.json())
+        .then(response => (this.submission.laughlink = response.audioUrl))
+        .then(() => alert("Laugh Uploaded. Don't forget to submit it 😉"));
+    },
+    stopRecorder() {
+      this.mediaRecorder.stop();
+      let recordLight = document.querySelector("#recordLight");
+      recordLight.style.background = "";
+      recordLight.style.color = "";
+      this.mediaRecorder.onstop = () => {
+        console.log("data available after MediaRecorder.stop() called.");
+        this.recordButton = false;
+        this.readyForUpload = true;
+        URL.revokeObjectURL(this.audioUrl);
+      };
+    },
+    recordAudio() {
+      if (navigator.mediaDevices) {
+        console.log("getUserMedia supported.");
+      }
+      this.recordButton = true;
+      navigator.mediaDevices
+        .getUserMedia({
+          audio: true,
+          video: false
+        })
+        .then(stream => {
+          this.mediaRecorder = new MediaRecorder(stream);
+          this.mediaRecorder.start();
+          let recordLight = document.querySelector("#recordLight");
+          recordLight.style.background = "red";
+          recordLight.style.color = "black";
+          this.mediaRecorder.ondataavailable = event => {
+            let audioChunks = [];
+            audioChunks.push(event.data);
+            this.audioBlob = new Blob(audioChunks, {
+              type: "audio/wav; codecs=opus"
+            });
+
+            let newURL = URL.createObjectURL(this.audioBlob);
+            let audio = document.querySelector("audio");
+            this.audioURL = newURL;
+            audio.src = this.audioURL;
+            audioChunks = [];
+          };
+        });
     },
     chooseMethod() {
       let select = document.querySelector("select");
       if (select.value === "URL") {
         this.url = true;
         this.findFile = false;
+        this.mic = false;
+        this.readyForUpload = false;
       } else if (select.value === "ChooseFile") {
         this.findFile = true;
+        this.url = false;
+        this.mic = false;
+        this.readyForUpload = false;
+      } else if (select.value === "Record") {
+        this.mic = true;
+        this.findFile = false;
         this.url = false;
       }
     }
   }
 };
 </script>
+
 <style scoped>
 div {
   margin: 3rem 5rem 1rem 5rem;
